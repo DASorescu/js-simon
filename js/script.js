3c44909fa8d070e09e60fe67948c3010b266c174
github_DASorescu <srsdln97@gmail.com>
2022-07-04 15:50:28 +0200

    stringa-di-numeri-casuali-da-js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,52 @@ Consigli del giorno:
 * Pensate prima in italiano.
 * Dividete in piccoli problemi la consegna.
 * Individuate gli elementi di cui avete bisogno per realizzare il programma.
+*/
+
+// preparazione div vuoti in html 
+// stilizzazione css 
+// rimozione contenuto dei div 
+// dichiarazione dei div in js attraverso get element
+
+
+
+
+// ? Preparazione variabili 
+
+const timer = document.getElementById('timer');
+const numString = document.getElementById('number-string');
+console.log( timer , numString);
+const numbersArray = [];
+
+
+
+
+// //FUNZIONI
+// funzione che crea numeri casuali 
+function randomNumbers(a , b ){
+    return Math.floor(Math.random() * (b) + a);
+}
+
+
+
+
+// ciclo per riempire l'array con i numeri casualmente generati 
+for(let i=0 ; i < 5 ; i++){
+
+    let newNumber ;
+    do{
+        let newNumber = randomNumbers(1,100);
+        numbersArray.push(newNumber);
+    }while(numbersArray.includes(newNumber));
+        
+}
+    
+// ciclo for per assegnare i valori dell'array al nostro elemento nel dom 
+let numbers = ''
+for(i = 0 ; i < numbersArray.length ; i++){
+    numbers += ` ${numbersArray[i]}`;
+}
+numString.innerText = numbers;
+
+    
+
